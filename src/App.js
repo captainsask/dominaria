0e6cb688a73413a9c001d0b01f56dbd7684fea69
Thomas Demkey <thomasd@criticalmass.com>
2017-04-27 23:45:56 -0600

    Committing initial grid outline. Very basic but should allow us to work with it.

--- src/App.js
+++ src/App.js
@@ -5,14 +5,44 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="dm-builder-app">
+        <div className="dm-builder-header">
+          {/*<img src={logo} className="App-logo" alt="logo" />*/}
+          <h2>Dungeon Builder</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p className="dm-builder-intro">
+          First thing to do is to create a grid. I'm looking at using foundation. For now though lets just roll our own.
         </p>
+        <Grid />
+        <Grid />
+        <Grid />
+        <Grid />
+        <Grid />
+        <Grid />
+      </div>
+    );
+  }
+}
+
+class Grid extends Component {
+  render() {
+    return(
+      <div className="dn-builder-grid container">
+        <div className="row">
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+        </div>
+        <div className="row">
+            <div className="row"></div>
+            <div className="row"></div>
+            <div className="row"></div>
+        </div>
+        <div className="row">
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+        </div>
       </div>
     );
   }
